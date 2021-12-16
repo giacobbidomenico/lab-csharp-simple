139be40064205d133e0d4e9691d58a2415c7313d
Domenico Giacobbi <domenico.giacobbi@studio.unibo.it>
2021-12-16 18:00:51 +0100

    update of Complex class in ComplexAlgebra project

--- 2-ComplexAlgebra/Complex.cs
+++ 2-ComplexAlgebra/Complex.cs
@@ -19,73 +19,44 @@ namespace ComplexAlgebra
     /// TODO:     - e.g. via the Equals(object) method
     public class Complex
     {
-        /// <summary>
-        /// 
-        /// </summary>
+        
         public double Real { get; }
-        /// <summary>
-        /// 
-        /// </summary>
+        
         public double Imaginary { get; }
-        /// <summary>
-        /// 
-        /// </summary>
+        
         public double Modulus =>
             Math.Sqrt(this.Real * this.Real + this.Imaginary * this.Imaginary);
-        /// <summary>
-        /// 
-        /// </summary>
+        
         public double Phase =>
             Math.Atan2(this.Imaginary, this.Real);
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="real"></param>
-        /// <param name="imaginary"></param>
+        
         public Complex(double real, double imaginary)
         {
             this.Real = real;
             this.Imaginary = imaginary;
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="num"></param>
-        /// <returns></returns>
+        
         public Complex Plus(Complex num) => 
             new Complex(this.Real + num.Real, this.Imaginary + num.Imaginary);
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
+        
         public Complex Complement() =>
             new Complex(this.Real, -this.Imaginary);
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="num"></param>
-        /// <returns></returns>
+        
         public Complex Minus(Complex num) =>
             new Complex(this.Real - num.Real, this.Imaginary - num.Imaginary);
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
+
         public override string ToString()
         {
-            if (this.Imaginary == 0.0) return this.Real.ToString(); 
-            if (this.Real == 0.0)
+            if (Imaginary == 0.0) return Real.ToString();
+            string signAndImaginary = Imaginary > 0 ? "+" : "";
+            signAndImaginary = Imaginary != 1 ? $"{signAndImaginary}{Imaginary}" : signAndImaginary;
+            if (Real == 0.0)
             {
-                return this.Imaginary + "i";
+                return $"{signAndImaginary}i";
             }
-            string sign = this.Imaginary > 0 ? "+" : ""; 
-            return this.Real + sign + this.Imaginary + "i";
+            return $"{Real}{signAndImaginary}i";
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <returns></returns>
+
         public override bool Equals(object obj)
         {
             if (ReferenceEquals(null, obj)) return false;
@@ -93,19 +64,12 @@ namespace ComplexAlgebra
             if (obj.GetType() != GetType()) return false;
             return Equals((Complex)obj);
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="complex"></param>
-        /// <returns></returns>
+
         public  bool Equals(Complex complex)
         {
             return this.Imaginary.Equals(complex.Imaginary) && this.Real.Equals(complex.Real);
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
+
         public override int GetHashCode() => 
             HashCode.Combine(this.Real, this.Imaginary);
     }
